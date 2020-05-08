87c7a503f2804ec6fec92ea4abd15e90ae5dc9e5
unknown <niru12376@gmail.com>
2020-05-08 23:42:56 +0530

    Hey there

--- python_code.py
+++ python_code.py
@@ -1,2 +1,13 @@
-print("Niranjan")
+#!/usr/bin/env python
+# coding: utf-8
+
+# Initial for Capstone 
+# 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np 
+print('Hello Capstone Project Course!')
 
